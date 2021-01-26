89e78c2986ac5b1ba2585d479690642644ca2af1
Felipe Cesar Soberanski <felepeipe@gmail.com>
2021-01-26 19:19:43 -0300

    Add files via upload

---
+++ Bank_Acc_Exercise/Class1.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+
+namespace Exercicio0
+{
+    class Conta
+    {
+        public int _contabanco;
+        public double Saldo;
+        public string Titular;
+
+        public Conta(double saldo) 
+        {
+            Saldo = saldo;
+        }
+        public Conta(string titular, int número)
+        {
+            _contabanco = número;
+            Titular = titular;
+        }
+
+        public Conta(string titular, int número, double saldo)
+        {
+            _contabanco = número;
+            Titular = titular;
+            Saldo = saldo;
+        } 
+    }
+}
---
+++ Bank_Acc_Exercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Exercicio0
+{
+    class ContaBancária
+    {
+        static void Main(string[] args)
+        {
+            string titular;
+            int número;
+            double dep;
+
+            Console.Write("Insira o nome do titular:");
+            titular = Console.ReadLine();
+
+            Console.Write("Insira o número da Conta:");
+            número = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Deseja realizar um depósito inicial? y/n");
+            string ini = Console.ReadLine();
+
+            Conta x = new Conta(titular, número, 0);
+
+            System.Console.WriteLine(x);
+
+            if (ini == "y")
+            {
+                Console.Write("Insira o valor a ser depositado:");
+                dep = double.Parse(Console.ReadLine());
+                _ = new Conta(dep);
+                Console.WriteLine(x);
+            }
+
+            Console.WriteLine(x);
+        }
+
+    }
+}
+
